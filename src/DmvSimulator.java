a98b90571d1d20efdd8261ecc6869372efa67578
kkachmar <71847954+kkachmar@users.noreply.github.com>
2021-03-02 17:55:46 -0500

    setup simulator inital commit

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,6 +1,32 @@
+
 public class DmvSimulator {
 
     public static void main(String[] args){
+        System.out.println("Welcome to the DMV!");
+        int number = (int)Math.random()*100;
+
+        System.out.println("Your number is: "+number+".");
+        System.out.println("Please wait until your number is called.");
+
+        number++;
+
+        for(int j = 0; j<100; j++){
+            if(number<100){
+                number++;
+            } else if (number == 100){
+                number = 0;
+                number++;
+            } else{
+                number++;
+            }
+
+            System.out.println("Now helping number: "+number);
+
+        }
+        System.out.println("You do not have the required paperwork. GOODBYE.");
+
+        System.exit(0);
+
 
     }
 }
